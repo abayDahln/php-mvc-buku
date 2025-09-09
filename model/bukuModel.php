6d54f0de5d6e4b797c497862a40d0164b8197ef5
abayDahln <abby11dahlan@gmail.com>
2025-09-09 07:22:16 +0700

    add buku model

---
+++ model/bukuModel.php
@@ -0,0 +1,16 @@
+<?php
+
+class bukuModel extends connection{
+
+    public function getAll(){
+        $query = $this->connect()->query("SELECT * FROM buku")->fetch_all(MYSQLI_ASSOC);
+        return $query;
+    }
+
+    public function getById($id){
+        $query = $this->connect()->query("SELECT * FROM buku WHERE id='$id'")->fetch_assoc();
+        return $query;
+    }
+}
+
+?>
